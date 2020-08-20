6b2958eb4c57dd2e68412f6cd447a1dc5afc512f
Diego Medina <com1pac56@hotmail.com>
2020-08-20 11:25:23 -0500

    DropNA

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -12,5 +12,6 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+        data = data.dropna()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
